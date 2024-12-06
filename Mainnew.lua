9ccf103405909ce315c13d9635f2a594d64b15bd
lvTaizz <leviettai182@gmail.com>
2024-12-06 19:44:51 +0700

    Add files via upload

--- Mainnew.lua
+++ Mainnew.lua
@@ -1158,7 +1158,7 @@ local AttackAnim = Instance.new("Animation")
 local AttackCoolDown = 0
 local cooldowntickFire = 0
 local MaxFire = 1000
-local FireCooldown = 0.009
+local FireCooldown = 0
 local FireL = 0
 local Fast_Attack = true
 local bladehit = {}
@@ -1298,7 +1298,7 @@ local function ChangeModeFastAttack(SelectFastAttackMode)
     elseif SelectFastAttackMode == "Fast Attack" then
         FireCooldown = 0.02
     elseif SelectFastAttackMode == "Taidz Fast" then
-        FireCooldown = 0
+        FireCooldown = 0.015
     end
 end
 
